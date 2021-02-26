82eae701a82ea329b8d032828002b98ab00374a0
luckyqzp <zeroqzp@163.com>
2021-02-26 19:54:30 +0800

    version  0.14 banner区域新增函数节流与定时器自动轮播js代码

--- js/carousel.js
+++ js/carousel.js
@@ -10,7 +10,8 @@
     var left_btn = document.getElementById('left_btn');
     var right_btn = document.getElementById('right_btn');
     var circle_ol = document.getElementById('circles_ol');
-    var circle_lis = circle_ol.getElementsByTagName('li')
+    var circle_lis = circle_ol.getElementsByTagName('li');
+    var banner = document.getElementById('banner');
 
     //克隆第一张li
     var clone_li = carousel_list.firstElementChild.cloneNode(true);
@@ -18,8 +19,18 @@
     carousel_list.appendChild(clone_li);
     //当前正在显示的图片序号，从0开始
     var idx = 0;
+
+    //节流锁
+    var lock = true;
     //右按钮事件监听
-    right_btn.onclick = function () {
+
+    right_btn.onclick = right_btn_handler;
+    //右按钮的事件处理函数
+    function right_btn_handler () {  
+        //判断节流锁的状态，如果是关闭的，那么就什么都不做
+        if(!lock) return;
+        //关锁
+        lock = false;
         //加上过渡
         carousel_list.style.transition = 'transform .5s ease 0s';
         //idx 要加1
@@ -39,9 +50,17 @@
         }
         //设置小圆点
         setCircles();
+        //开锁，动画结束之后开锁
+        setTimeout(function(){
+            lock = true;
+        },500)
     }
     // 左按钮的事件监听
     left_btn.onclick = function () {
+        //判断节流锁的状态，如果是关闭的，那么就什么都不做
+        if(!lock) return;
+        //关锁
+        lock = false;
         //左按钮很特殊，要先写if语句，而不是idx--
         if (idx == 0) {
             //瞬间拉动到最后
@@ -66,6 +85,10 @@
         }
         //设置小圆点
         setCircles();
+        //开锁，动画结束之后开锁
+        setTimeout(function(){
+            lock = true;
+        },500)
     }
     //设置小圆点的current在谁身上，序号为idx的小圆点才有current类名，其他的li都没有类名
     function setCircles() {
@@ -96,5 +119,20 @@
             //调用改变小圆点函数
             setCircles();
         }
+    }
+
+    //定时器,自动轮播
+    var timer = setInterval(right_btn_handler, 2000);
+        
+    //鼠标进入，自动轮播暂停
+    banner.onmouseenter = function(){
+        clearInterval(timer);
+    }
+    //鼠标离开，自动轮播开始
+    banner.onmouseleave = function(){
+        //设表先关
+        clearInterval(timer);
+        //设置定时器，这里不要加var,加var就变为局部变量了,就clear不掉了！
+        timer = setInterval(right_btn_handler, 2000);
     }
 })();
